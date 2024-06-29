b3f5b0ee0feea540792678ea90371c2c8e20aac8
JennaFi <filimonova.evgeniya@gmail.com>
2024-06-29 05:18:56 +0300

    time function added

--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,28 @@
+from datetime import datetime
+
 from src.masks import get_mask_card_number, get_mask_account
 
 
-def mask_account_card(user_input: str) -> str | None:
-    """Функция общей маскировки карты и счета"""
+# def mask_account_card(user_input: str) -> str | None:
+#     """Функция общей маскировки карты и счета"""
+#
+#     symbols = user_input.split()
+#
+#     if symbols[0] == "Счет":
+#         symbols[-1] = get_mask_account(symbols[-1])
+#
+#     else:
+#         symbols[-1] = get_mask_card_number(symbols[-1])
+#
+#     return "".join(symbols)
+#
 
-    symbols = user_input.split()
+def get_data(user_input: str) -> str:
+    '''Функция преобразования даты'''
 
-    if symbols[0] == 'Счет':
-        symbols[-1] = get_mask_account(symbols[-1])
+    new_data = datetime.strptime(user_input, "%Y-%m-%dT%H:%M:%S.%f")
 
-    else:
-        symbols[-1] = get_mask_card_number(symbols[-1])
-
-    return ''.join(symbols)
+    return new_data.strftime("%d.%m.%Y")
 
 
-print(mask_account_card('Visa Platinum 8990922113665229'))
+print(get_data('2018-07-11T02:26:18.671407'))
